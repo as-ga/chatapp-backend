04ae1952b4ea1e72bd111ebaa8bd0baed30a2ad6
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-03-29 22:01:26 +0000

    Add corsOptions constant

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,14 @@
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+
 const DB_NAME = "gaurav-chat-app";
 
 const CHAT_TOKEN = "chat-token";
@@ -20,6 +31,7 @@ const CHAT_LEAVED = "CHAT_LEAVED";
 const ONLINE_USERS = "ONLINE_USERS";
 
 export {
+  corsOptions,
   DB_NAME,
   CHAT_TOKEN,
   ALERT,
