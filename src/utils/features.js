fda211d7401c41b244f6f5ccb864678532bcb95a
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-03-29 20:22:45 +0000

    Add features.js file with utility functions

---
+++ src/utils/features.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import { getSockets } from "./helper.js";
+
+const cookieOptions = {
+  maxAge: 15 * 24 * 60 * 60 * 1000,
+  sameSite: "none",
+  httpOnly: true,
+  secure: true,
+};
+
+const sendToken = (res, user, code, message) => {
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+  return res.status(code).cookie("chat-token", token, cookieOptions).json({
+    success: true,
+    user,
+    message,
+  });
+};
+
+const emitEvent = (req, event, users, data) => {
+  const io = req.app.get("io");
+  const usersSocket = getSockets(users);
+  io.to(usersSocket).emit(event, data);
+};
+
+export { sendToken, cookieOptions, emitEvent };
