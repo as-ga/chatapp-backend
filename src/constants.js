555c028f8cf3f781527ff00820c8ba966fabf33f
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-03-29 18:37:09 +0000

    Add constants for chat functionality

---
+++ src/constants.js
@@ -0,0 +1,45 @@
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+const CHAT_TOKEN = "chat-token";
+
+const ALERT = "ALERT";
+const REFETCH_CHATS = "REFETCH_CHATS";
+
+const NEW_ATTACHMENT = "NEW_ATTACHMENT";
+const NEW_MESSAGE_ALERT = "NEW_MESSAGE_ALERT";
+
+const NEW_REQUEST = "NEW_REQUEST";
+const NEW_MESSAGE = "NEW_MESSAGE";
+
+const START_TYPING = "START_TYPING";
+const STOP_TYPING = "STOP_TYPING";
+
+const CHAT_JOINED = "CHAT_JOINED";
+const CHAT_LEAVED = "CHAT_LEAVED";
+
+const ONLINE_USERS = "ONLINE_USERS";
+
+export {
+    corsOptions,
+    CHAT_TOKEN,
+    ALERT,
+    REFETCH_CHATS,
+    NEW_ATTACHMENT,
+    NEW_MESSAGE_ALERT,
+    NEW_REQUEST,
+    NEW_MESSAGE,
+    START_TYPING,
+    STOP_TYPING,
+    CHAT_JOINED,
+    CHAT_LEAVED,
+    ONLINE_USERS,
+    
+}
