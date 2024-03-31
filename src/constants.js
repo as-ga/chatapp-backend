513ba3a3f26c46f72d843794f3e9bbfa895b4ef6
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-03-31 15:08:08 +0000

    Update corsOptions in constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,5 @@
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:4173",
-    "http://localhost:3000",
-    process.env.CLIENT_URL,
-  ],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
